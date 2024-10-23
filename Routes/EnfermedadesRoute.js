215e847f2e51f2c5c10d65feea7c1694b032472e
NoelGom <159956865+NoelGom@users.noreply.github.com>
2024-10-22 21:23:41 -0600

    Update EnfermedadesRoute.js
    Ruta de la funcion de enfermedades mas comunes

--- Routes/EnfermedadesRoute.js
+++ Routes/EnfermedadesRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { readAll, createOne, deleteOne, updateOne, searchAll } = require('../Controllers/EnfermedadesController');
+const { readAll, createOne, deleteOne, updateOne, searchAll, getEnfermedadesComunes } = require('../Controllers/EnfermedadesController');
 
 
 const routes = express();
@@ -11,5 +11,5 @@ routes.put('/Enfermedades/:id', updateOne);
 routes.delete('/Enfermedades/:id',  deleteOne);
 routes.get('/Enfermedades', readAll);
 routes.get('/Enfermedades/buscar',  searchAll);
-
+routes.get('/Enfermedades/', getEnfermedadesComunes);
 module.exports = routes;
